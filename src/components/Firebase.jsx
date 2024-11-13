a8deff05846f358b5a34c61f9bf8a761909c3e63
Seckin Ozturk <franseck102@gmail.com>
2024-11-13 07:29:55 -0500

    Auth configs

--- src/components/Firebase.jsx
+++ src/components/Firebase.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
 
-const Firebase = () => {
-  return (
-    <div>Firebase</div>
-  )
-}
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-export default Firebase
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBVBPtv61eVpy18oTwAx3DbXx1ktRvoKlc",
+  authDomain: "first-app-1d1d0.firebaseapp.com",
+  projectId: "first-app-1d1d0",
+  storageBucket: "first-app-1d1d0.firebasestorage.app",
+  messagingSenderId: "937345906870",
+  appId: "1:937345906870:web:c12b91555ff5706b5fce17"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
