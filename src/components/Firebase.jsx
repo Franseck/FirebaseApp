499887a00a7bcc3396e0ed2e14522a563a0eaabe
Seckin Ozturk <franseck102@gmail.com>
2024-11-15 20:25:52 -0500

    firebase config

--- src/components/Firebase.jsx
+++ src/components/Firebase.jsx
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBVBPtv61eVpy18oTwAx3DbXx1ktRvoKlc",
-  authDomain: "first-app-1d1d0.firebaseapp.com",
-  projectId: "first-app-1d1d0",
-  storageBucket: "first-app-1d1d0.firebasestorage.app",
-  messagingSenderId: "937345906870",
-  appId: "1:937345906870:web:c12b91555ff5706b5fce17"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
